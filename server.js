e1e7cd6b45966fa51d999b9b7c3c2df0857eb217
thuan <nauhtyud@gmail.com>
2021-08-03 03:13:12 +0700

    chore: add public ip v4

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const geoip = require("geoip-lite");
 const ipaddr = require("ipaddr.js");
+const publicIp = require("public-ip");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const ipv4 = ipaddr.process(req.ip).toString();
+  const publicIpv4 = await publicIp.v4();
   const remoteAddress = req.connection.remoteAddress;
 
   // const geo1 = geoip.lookup("207.97.227.239");
   // const geo2 = geoip.lookup("10.71.221.241");
-
   const geo = geoip.lookup(ipv4);
+  const geoPublic = geoip.lookup(publicIpv4);
 
-  res.send(JSON.stringify({ ipv4, remoteAddress, geo }));
+  res.send(JSON.stringify({ ipv4, publicIpv4, remoteAddress, geo, geoPublic }));
 });
 
 app.listen(port, () => {
