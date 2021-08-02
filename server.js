397f125615737aaf82f4ad3ff7a6de9c7c8b395e
thuan <nauhtyud@gmail.com>
2021-08-03 03:56:21 +0700

    fix: key name

--- server.js
+++ server.js
@@ -10,29 +10,31 @@ const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
   const ipv4 = ipaddr.process(req.ip).toString();
-  const parseIp = req.headers["x-forwarded-for"]?.split(",").shift();
-  const remoteAddress1 = req.socket?.remoteAddress;
-  const remoteAddress2 = req.connection.remoteAddress;
+  const ipv4Geo = geoip.lookup(ipv4);
 
-  const clientIp = requestIp.getClientIp(req);
+  const socketRemoteAddress = req.socket?.remoteAddress;
+  const connectionRemoteAddress = req.connection.remoteAddress;
+
+  const xForwardedForIp = req.headers["x-forwarded-for"]?.split(",").shift();
+  const xForwardedForIpGeo = geoip.lookup(xForwardedForIp);
+  const getClientIp = requestIp.getClientIp(req);
+  const getClientIpGeo = geoip.lookup(getClientIp);
 
   const publicIpv4 = await publicIp.v4();
-
-  // const geo1 = geoip.lookup("207.97.227.239");
-  // const geo2 = geoip.lookup("10.71.221.241");
-  const geo = geoip.lookup(ipv4);
-  const geoPublic = geoip.lookup(publicIpv4);
+  const publicIpv4Geo = geoip.lookup(publicIpv4);
 
   res.send(
     JSON.stringify({
       ipv4,
-      clientIp,
-      parseIp,
+      ipv4Geo,
+      socketRemoteAddress,
+      connectionRemoteAddress,
+      xForwardedForIp,
+      xForwardedForIpGeo,
+      getClientIp,
+      getClientIpGeo,
       publicIpv4,
-      remoteAddress1,
-      remoteAddress2,
-      geo,
-      geoPublic,
+      publicIpv4Geo,
     })
   );
 });
